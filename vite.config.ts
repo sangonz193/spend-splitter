1219dac4b39208f7e312563c29b4fb449954560c
Santiago Gonzalez <37671738+sangonz193@users.noreply.github.com>
2024-05-11 23:22:51 -0300

    configure tailwind + shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/spend-splitter",
+	// base: "/spend-splitter",
 	plugins: [react()],
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
 	},
 })
